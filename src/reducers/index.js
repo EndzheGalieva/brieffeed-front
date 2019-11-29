892095dcab2e3279bf714f2db348af6820089ba1
Airat Galiev <airat.galiev@gmail.com>
2019-11-29 12:05:20 +0300

    добавил редюсер и экшен для безопасности

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import errorReducer from './errorReducer';
 import categoryReducer from './categoryReducer';
 import blogReducer from './blogReducer';
 import postReducer from './postReducer';
+import securityReducer from './securityReducer';
 
 export default combineReducers({
   category: categoryReducer,
   blog: blogReducer,
   post: postReducer,
+  security: securityReducer,
   errors: errorReducer
 });
